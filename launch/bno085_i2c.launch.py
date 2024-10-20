61d57fc51116f3a5c9b5fc9cec1598133ee39e6b
bnbhat <balachandra.bhat@lemvos.com>
2024-10-21 00:50:37 +0200

    add launch and config files

---
+++ launch/bno085_i2c.launch.py
@@ -0,0 +1,21 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    config = os.path.join(
+        get_package_share_directory('bno08x_ros2_driver'), 
+        'config',
+        'bno085_i2c.yaml'
+    )
+
+    return LaunchDescription([
+        Node(
+            package='bno08x_ros2_driver',  
+            executable='bno08x_driver',  
+            name='imu_driver',
+            output='screen',
+            parameters=[config]
+        ),
+    ])
